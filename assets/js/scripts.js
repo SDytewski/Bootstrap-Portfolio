ed9a0ca306da638f9e61e71eeea54dd5c90d11e5
Steven Dytewski <40008730+Dy2ski@users.noreply.github.com>
2019-06-14 16:46:24 -0700

    fixed hamburger menu in contact and changed animation bullet

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,25 +2,7 @@
 var words = ["<div>ANIMATIONS</div>", "<div>S T Y L I N G</div>", "<div>MOBILE</div>", "<div>APPLICATIONS</div>", "<div>RELIABLE</div>", "<div>FUNCTIONALITY</div>"];
 var count = 0;
 var interval = setInterval(function(){
-    $("#rotate_word").css({"font-weight" : "600", "font-size" : "25px", "-webkit-text-stroke": ".4px black", "font-family" : "georgia", "letter-spacing": ".18rem", "text-shadow" : "0 0 16px grey",  "font-style" : "italic", "background-image" : "linear-gradient(to right, rgba(255,0,0,0), #ff8080", "border-radius":"25px 10px 35px 10px" }).html(words[count++ % words.length])},
+    $("#rotate_word").css({"font-weight" : "600", "font-size" : "25px", "-webkit-text-stroke": ".4px black", "font-family" : "georgia", "letter-spacing": ".18rem", "text-shadow" : "0 0 16px grey",  "font-style" : "italic", "background-image" : "linear-gradient(to right, rgba(255,0,0,0), #ff8080", "border-radius":"25px 10px 35px 10px", "margin-bottom" : "20px", "margin-left" : "-16px"}).html(words[count++ % words.length])},
     1200)
   ;
 
-// for (var i = 0; i < words.length; i++){
-
-// $("#rotate_word").html(words);
-
-
-// }
-
-// setInterval(function rotateWord(){
-
-//     for (var i = 0; i < words.length; i++){
-
-//         $("#rotate_word").html(words[i]);
-   
-//         }
-// }, 2000)
-// $(document).ready(function(){
-//     $( ":header" ).css( "background-color", "#bbbbff" );
-//   });
