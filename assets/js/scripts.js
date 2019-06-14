7926412a2bae536043cefa32abd3af5f1d9319d5
Steven Dytewski <40008730+Dy2ski@users.noreply.github.com>
2019-06-14 16:10:48 -0700

    changed array words in contact page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
   //Create an array
-var words = ["<div>ANIMATIONS</div>", "<div>S T Y L I N G</div>", "<div>MOBILE-RESPONSIVENESS</div>", "<div>USER-FRIENDLY APPLICATIONS</div>", "<div>FUNCTIONALITY</div>","<div>RELIABLE</div>"];
+var words = ["<div>ANIMATIONS</div>", "<div>S T Y L I N G</div>", "<div>MOBILE</div>", "<div>APPLICATIONS</div>", "<div>RELIABLE</div>", "<div>FUNCTIONALITY</div>"];
 var count = 0;
 var interval = setInterval(function(){
     $("#rotate_word").css({"font-weight" : "600", "font-size" : "25px", "-webkit-text-stroke": ".4px black", "font-family" : "georgia", "letter-spacing": ".18rem", "text-shadow" : "0 0 16px grey",  "font-style" : "italic", "background-image" : "linear-gradient(to right, rgba(255,0,0,0), #ff8080", "border-radius":"25px 10px 35px 10px" }).html(words[count++ % words.length])},
